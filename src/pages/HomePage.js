a9624d95d7020bec57f62043a6f7c5598590fb95
JuaMV <juan.menares.vega@ciisa.cl>
2023-03-11 23:38:32 -0300

    Header fixed y ahora se muestra el ultimo post agregado primero

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,7 +17,7 @@ export const HomePage = () => {
 
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-10">
-        {posts.map((post) => {
+        {[...posts].reverse().map((post) => {
           return <PostCard post={post} key={post._id} />;
         })}
       </div>
@@ -26,7 +26,7 @@ export const HomePage = () => {
 
   return (
     <div className="text-white">
-      <header className="flex justify-between py-4">
+      <header className="flex justify-between p-5 sticky top-0 z-50 mb-3 -mx-1 rounded-b-lg bg-neutral-900 bg-opacity-95">
         <h1 className="text-2xl text-gray-300 font-bold">
           Post ({posts.length})
         </h1>
